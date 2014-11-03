31917d71b89c3b211a86b345b82b84cbeb90c386
Jason Gegere <jason@hgmail.com>
2014-11-03 16:56:56 -0600

    Deployment steps

--- Docker/Apache/notes.sh
+++ Docker/Apache/notes.sh
@@ -1,3 +1,3 @@
 docker build -t htmlgraphic/apache .
 
-docker run -pd 80:80 -v /Users/gegere/Dropbox/SITES:/var/www/html --name apache 98bc68337f5a
+docker run -pd 80:80 --link mysql:mysql -v /Users/gegere/Dropbox/SITES:/var/www/html --name apache htmlgraphic/apache
