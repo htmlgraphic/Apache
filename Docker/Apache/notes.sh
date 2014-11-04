a66560fe7f11c3976f34dbcef88304e5cece7991
Jason Gegere <jason@hgmail.com>
2014-11-04 11:57:21 -0600

    attached Apache to MySQL, increase server build #

--- Docker/Apache/notes.sh
+++ Docker/Apache/notes.sh
@@ -1,3 +1,3 @@
 docker build -t htmlgraphic/apache .
 
-docker run -pd 80:80 --link mysql:mysql -v /Users/gegere/Dropbox/SITES:/var/www/html --name apache htmlgraphic/apache
+docker run -pd 80:80 --link mysql:mysql --volumes-from www-data -v /var/public_html:/var/www/html --name apache htmlgraphic/apache
