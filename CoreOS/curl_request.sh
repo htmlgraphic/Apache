4513e7fe15432d34bcb546a822c5d07c5d1550fa
Jason Gegere <jason@hgmail.com>
2014-11-04 12:00:07 -0600

    using CoreOS Alpha

--- CoreOS/curl_request.sh
+++ CoreOS/curl_request.sh
@@ -1,4 +1,4 @@
 curl -X POST "https://api.digitalocean.com/v2/droplets" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer f7caadcd3e3c2cb9f501479aa287ec9061bb33b1eb654d77f077a3e698781974" \
--d'{"name":"coreos-6","region":"nyc3","size":"1GB","private_networking":true,"image":"coreos-stable", "user_data": "'"$(cat cloud-config.yaml | sed 's/"/\\"/g')"'", "ssh_keys":[98825]}'
+-d'{"name":"coreos-7","region":"nyc3","size":"1GB","private_networking":true,"image":"coreos-alpha", "user_data": "'"$(cat cloud-config.yaml | sed 's/"/\\"/g')"'", "ssh_keys":[98825]}'
