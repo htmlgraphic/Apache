c5221cd41b2d866523d2117ac21460c000127c94
Jason Gegere <jason@hgmail.com>
2014-11-04 11:58:30 -0600

    mysql deploy

---
+++ Docker/MySQL/notes.sh
@@ -0,0 +1,24 @@
+docker build -t htmlgraphic/mysql .
+docker run -d --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=U7eWsrqETxy2V htmlgraphic/mysql
+
+
+#CONNECT TO MYSQL VIA BASH
+docker run -it --rm --link mysql:mysql htmlgraphic/mysql bash -c 'mysql -h $MYSQL_PORT_3306_TCP_ADDR'
+
+
+
+
+
+
+
+docker run -pd 3306:3306 --volumes-from dbdata -v /Users/gegere/Dropbox/SITES:/var/www/html --name mysql 7b8aa46407cc
+
+
+
+
+#DATA CONTAINER
+docker run -div /var/www/public_html --name www-data htmlgraphic/www-data
+
+
+
+docker run -d --volumes-from dbdata --name db2 IMAGE_ID
