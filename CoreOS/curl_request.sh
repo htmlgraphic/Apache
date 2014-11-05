7ef4c0fc5df589f564b78f4073df82a590d0e277
Jason Gegere <jason@hgmail.com>
2014-11-05 11:19:03 -0600

    private region setup

--- CoreOS/curl_request.sh
+++ CoreOS/curl_request.sh
@@ -1,4 +1,4 @@
 curl -X POST "https://api.digitalocean.com/v2/droplets" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $DO_TOKEN" \
--d'{"name":"coreos-18","region":"sfo1","size":"1GB","private_networking":true,"image":"coreos-alpha", "user_data": "'"$(cat cloud-config.yaml | sed 's/"/\\"/g')"'", "ssh_keys":[98825]}'
+-d'{"name":"coreos-19","region":"nyc3","size":"1GB","private_networking":true,"image":"coreos-alpha", "user_data": "'"$(cat cloud-config.yaml | sed 's/"/\\"/g')"'", "ssh_keys":[98825]}'
