2357ecf45beb7fca45f40fb9f33e1781a0b1d5b4
Jason Gegere <jason@hgmail.com>
2014-11-13 02:23:34 -0600

    Apache container loading temp page, for now

--- Docker/Apache/run.sh
+++ Docker/Apache/run.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-if [ ! -f /data/www/conf ]; then
+echo 10.132.191.193 mysql >> /etc/hosts
+
+if [ ! -d /data/apache2 ]; then
 	
-	mkdir /data/www
-	mkdir /data/www/conf
+	mkdir -p /data/apache2
 
-	# Symlink default directory, for apache conf scripts
-	ln -s /etc/apache2/sites-enabled /data/www/conf
+	# Move initial apache conf script into directory
+	cp -R /etc/apache2/* /data/apache2
 
-	cp apache-config.conf /data/www/conf/000-default.conf
+	# Set the 'ServerName' directive globally
+	echo ServerName localhost >> /data/apache2/conf-available/servername.conf
+
+	# Move default htmlgraphic conf from /opt
+	#mv /opt/apache-config.conf /data/apache2/sites-enabled/htmlgraphic.conf
+
+	# Disable the default website
+	rm /data/apache2/sites-enabled/000-default.conf
 
 fi
 
-source /etc/apache2/envvars
-exec /usr/sbin/apache2ctl -D FOREGROUND
+/bin/bash -c "source /data/apache2/envvars"
+/bin/bash -c "etc/init.d/apache2 start"
+
+#exec /usr/sbin/apache2ctl -D FOREGROUND
+
+while ( true )
+    do
+    echo "Detach with Ctrl-p Ctrl-q. Dropping to shell"
+    sleep 1
+    /bin/bash
+done
