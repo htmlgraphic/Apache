8a0fabc6465934af9cdcebb5cd14152014e841c5
Jason Gegere <jason@hgmail.com>
2014-11-18 18:19:49 -0600

    pass user/pass to Postfix for delivery over SendGrid

--- Docker/Postfix/start.sh
+++ Docker/Postfix/start.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+
+sudo postconf -e \
+   myhostname=post-office.htmlgraphic.com \
+   myorigin=\$mydomain \
+   mydestination="localhost.localdomain localhost" \
+   mynetworks="10.132.0.0/16 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128" \
+   mail_spool_directory="/var/spool/mail/" \
+   smtp_sasl_auth_enable=yes \
+   smtp_sasl_password_maps=static:$USER:$PASS \
+   smtp_sasl_security_options=noanonymous \
+   smtp_tls_security_level=encrypt \
+   header_size_limit=4096000 \
+   relayhost=[smtp.sendgrid.net]:587
+
+
 # These are required when postfix runs chrooted
 #
 [[ -z $(ls /var/spool/postfix/etc) ]] && {
