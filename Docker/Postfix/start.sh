3950ce24ec6a7392ddf0b75514beab0ad04a58c6
Jason Gegere <jason@hgmail.com>
2014-11-18 22:45:36 -0600

    initial build ready for the postfix instance

--- Docker/Postfix/start.sh
+++ Docker/Postfix/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# myhostname should match the name that is given to the container via the 'docker run' command. 
+# This will help any internal email route proper outbound.
 postconf -e \
    myhostname=post-office.htmlgraphic.com \
    mydomain=htmlgraphic.com \
    mydestination="localhost.localdomain localhost" \
-   mynetworks="10.132.0.0/16 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128" \
+   mynetworks="104.236.0.0/18, 10.132.0.0/16 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128" \
    mail_spool_directory="/var/spool/mail/" \
+   virtual_alias_maps=hash:/etc/postfix/virtual \
    smtp_sasl_auth_enable=yes \
    smtp_sasl_password_maps=static:$USER:$PASS \
    smtp_sasl_security_options=noanonymous \
