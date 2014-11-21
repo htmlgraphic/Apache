72c93029c5aad77446ee471236adf6e5e7dcb229
Jason Gegere <jason@hgmail.com>
2014-11-21 13:28:00 -0600

    node creation process

--- CoreOS/create_node.sh
+++ CoreOS/create_node.sh
@@ -1,4 +1,7 @@
+# Create a new node on Digital Ocean
+# By Jason Gegere <jason@htmlgraphic.com>
+
 curl -X POST "https://api.digitalocean.com/v2/droplets" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $DO_TOKEN" \
--d'{"name":"coreos61","region":"nyc3","size":"512mb","private_networking":true,"image":"coreos-stable", "user_data": "'"$(cat cloud-config.yaml | sed 's/"/\\"/g')"'", "ssh_keys":[98825]}'
+-d'{"name":"coreos61","region":"nyc3","size":"1gb","private_networking":true,"image":"coreos-stable", "user_data": "'"$(cat cloud-config.yaml | sed 's/"/\\"/g')"'", "ssh_keys":[98825]}'
