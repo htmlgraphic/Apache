fe1a0d6f504a2bd9ebbbc3e7daf1063fb1aea7d1
Jason Gegere <jason@hgmail.com>
2014-11-21 20:35:12 -0600

    add mysql to hosts on creation

--- Docker/Data/run.sh
+++ Docker/Data/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 
+echo 10.132.191.193 mysql >> /etc/hosts
+
 # Postfix use smart host to relay email
 postconf -e \
     relayhost=[post-office.htmlgraphic.com]:25 \
     inet_protocols=ipv4
+
 
 
 /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
