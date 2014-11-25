963b7dfff58cb7c624d195b6f85bc2ead68ca5f5
Jason Gegere <jason@hgmail.com>
2014-11-25 03:54:08 -0600

    enable backups

--- CoreOS/create_node.sh
+++ CoreOS/create_node.sh
@@ -4,4 +4,4 @@
 curl -X POST "https://api.digitalocean.com/v2/droplets" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $DO_TOKEN" \
--d'{"name":"coreos61","backups":true,"region":"nyc3","size":"1gb","private_networking":true,"image":"coreos-stable", "user_data": "'"$(cat cloud-config.yaml | sed 's/"/\\"/g')"'", "ssh_keys":[98825]}'
+-d'{"name":"coreos62","backups":true,"region":"nyc3","size":"1gb","private_networking":true,"image":"coreos-stable", "user_data": "'"$(cat cloud-config.yaml | sed 's/"/\\"/g')"'", "ssh_keys":[98825]}'
