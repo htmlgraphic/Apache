6cdd29fdaabebc28702169d4df17830880052a46
Jason Gegere <jason@hgmail.com>
2014-12-10 13:06:28 -0600

    adding tweaks and notes into build

--- Docker/MySQL/run-client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-TAG="mysql"
-
-CONTAINER_ID=$(docker ps | grep $TAG | awk '{print $1}')
-
-IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $CONTAINER_ID)
-
-mysql -u admin -p -h $IP
