ba5bf3a0ad351a5a9a465bdffcf4e653ab791dee
Jason Gegere <jason@hgmail.com>
2014-12-12 21:28:45 -0600

    moving CoreOS files into seperate repo

--- CoreOS/create_node.sh
+++
@@ -1,7 +0,0 @@
-# Create a new node on Digital Ocean
-# By Jason Gegere <jason@htmlgraphic.com>
-
-curl -X POST "https://api.digitalocean.com/v2/droplets" \
--H "Content-Type: application/json" \
--H "Authorization: Bearer $DO_TOKEN" \
--d'{"name":"coreos62","backups":true,"region":"nyc3","size":"1gb","private_networking":true,"image":"coreos-stable", "user_data": "'"$(cat cloud-config.yaml | sed 's/"/\\"/g')"'", "ssh_keys":[98825]}'
