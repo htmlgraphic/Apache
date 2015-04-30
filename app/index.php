029ca1bc7aed24d6e787c43bf58832227fc7eac9
Jason Gegere <jason@htmlgraphic.com>
2015-04-29 21:58:51 -0600

    tweak shippable.yml, update the path of a test web page for local develop

--- app/index.php
+++ app/index.php
@@ -1,4 +1,9 @@
 <h2>Coming soon!</h2>
-<!--
-Hello, come and find me... /data/www/public_html/index.php
--->
+
+<pre>
+This <strong>index.php</strong> file is located within the following directory /data/www/public_html/
+
+More build instructions will follow to turn this into a simple turn key solution to test and manage your local development.
+</pre>
+
+<?php //phpinfo(); ?>
