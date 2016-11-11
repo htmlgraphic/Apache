3ecac63c877f4019e96d0aea5720f35062a25715
Jason Gegere <jason@htmlgraphic.com>
2016-11-11 02:04:40 -0700

    updating paths for PHP 7

--- app/run.sh
+++ app/run.sh
@@ -59,46 +59,47 @@ fi
 #  Edit files on the instance, check for proper environment
 #
 #####
-if [ ! -f /etc/php5/apache2/build ]; then
+if [ ! -f /etc/php/7.0/apache2/build ]; then
 
 	# Tweak Apache build
-	sed -i 's|;include_path = ".:/usr/share/php"|include_path = ".:/usr/share/php:/data/pear"|g' /etc/php5/apache2/php.ini
-	sed -i 's/variables_order.*/variables_order = \"EGPCS\"/g' /etc/php5/apache2/php.ini
+	sed -i 's|;include_path = ".:/usr/share/php"|include_path = ".:/usr/share/php:/data/pear"|g' /etc/php/7.0/apache2/php.ini
+	sed -i 's/variables_order.*/variables_order = \"EGPCS\"/g' /etc/php/7.0/apache2/php.ini
 	sed -i 's/IncludeOptional sites-enabled\/\*.conf/IncludeOptional \/data\/apache2\/sites-enabled\/*.conf/' /etc/apache2/apache2.conf
+	sed -i 's|;error_log = php_errors.log|error_log = /data/apache2/logs/error_log|g' /etc/php/7.0/apache2/php.ini
 
 	# Update the PHP.ini file, enable <? ?> tags and quiet logging.
-	sed -i 's|short_open_tag = Off|short_open_tag = On|g' /etc/php5/apache2/php.ini
-	sed -i 's|;session.save_path = "/var/lib/php5"|session.save_path = "/tmp"|g' /etc/php5/apache2/php.ini
+	sed -i 's|short_open_tag = Off|short_open_tag = On|g' /etc/php/7.0/apache2/php.ini
+	sed -i 's|;session.save_path = "/var/lib/php5"|session.save_path = "/tmp"|g' /etc/php/7.0/apache2/php.ini
 
 	# Increase upload file limitations
-	sed -i 's|upload_max_filesize = 2M|upload_max_filesize = 1000M|g' /etc/php5/apache2/php.ini
-	sed -i 's|post_max_size = 8M|post_max_size = 1000M|g' /etc/php5/apache2/php.ini
+	sed -i 's|upload_max_filesize = 2M|upload_max_filesize = 1000M|g' /etc/php/7.0/apache2/php.ini
+	sed -i 's|post_max_size = 8M|post_max_size = 1000M|g' /etc/php/7.0/apache2/php.ini
 
 	# Allow the container to continuously update it's time
 	echo "ntpdate ntp.ubuntu.com" > /etc/cron.daily/ntpdate && chmod 755 /etc/cron.daily/ntpdate
 
 	# Add imagick extension
-	echo "extension=imagick.so" >> /etc/php5/apache2/php.ini
+	echo "extension=imagick.so" >> /etc/php/7.0/apache2/php.ini
 
 
 	# Add build file to remove duplicate script execution
-	echo 1 > /etc/php5/apache2/build
+	echo 1 > /etc/php/7.0/apache2/build
 
 	if [[ ! -z "${NODE_ENVIRONMENT}" ]]; then
 
 			if [ "$NODE_ENVIRONMENT" == 'dev' ]; then
 					# Tweak Apache build
-					sed -i 's|\[PHP\]|\[PHP\] \nIS_LIVE=0 \nIS_DEV=0 \nNODE_ENVIRONMENT=dev \n;The IS_DEV is set for testing outside of DEV environments ie: test.domain.tld|g' /etc/php5/apache2/php.ini
+					sed -i 's|\[PHP\]|\[PHP\] \nIS_LIVE=0 \nIS_DEV=0 \nNODE_ENVIRONMENT=dev \n;The IS_DEV is set for testing outside of DEV environments ie: test.domain.tld|g' /etc/php/7.0/apache2/php.ini
 					# Update the PHP.ini file, enable <? ?> tags and quiet logging.
-					sed -i "s/error_reporting = .*$/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+					sed -i "s/error_reporting = .*$/error_reporting = E_ALL/" /etc/php/7.0/apache2/php.ini
 			fi
 
 
 			if [ "$NODE_ENVIRONMENT" == 'production' ]; then
 					# Tweak Apache build
-					sed -i 's|\[PHP\]|\[PHP\] \nIS_LIVE=1 \nIS_DEV=0 \nNODE_ENVIRONMENT=production \n;The IS_DEV is set for testing outside of DEV environments ie: test.domain.tld|g' /etc/php5/apache2/php.ini
+					sed -i 's|\[PHP\]|\[PHP\] \nIS_LIVE=1 \nIS_DEV=0 \nNODE_ENVIRONMENT=production \n;The IS_DEV is set for testing outside of DEV environments ie: test.domain.tld|g' /etc/php/7.0/apache2/php.ini
 					# Update the PHP.ini file, enable <? ?> tags and quiet logging.
-					sed -i "s/error_reporting = .*$/error_reporting = E_ERROR | E_WARNING | E_PARSE/" /etc/php5/apache2/php.ini
+					sed -i "s/error_reporting = .*$/error_reporting = E_ERROR | E_WARNING | E_PARSE/" /etc/php/7.0/apache2/php.ini
 			fi
 
 	else
